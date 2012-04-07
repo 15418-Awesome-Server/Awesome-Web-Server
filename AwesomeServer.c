b2d3fe3383045992de375c8cfb5eea5ed42b5cb1
Eric <esummers@andrew.cmu.edu>
2012-04-07 18:12:07 -0400

    main and structs

--- AwesomeServer.c
+++ AwesomeServer.c
@@ -1,3 +1,29 @@
+void doit(int fd);
+void read_requestedrs(rio_t *rp);
+int pare_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filenam, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+
+struct in_addr{
+	unsigned int 	s_addr; 		//Network byte order (Big-endian)
+};
+
+struct sockaddr{	//generic
+	unsigned short 	sa_family;		//Protocol family
+	char			sa_data[14];	//Addr data
+};
+
+struct sockaddr_in{	//internet
+	unsigned short 	sin_family; 	//Address Family
+	unsigned short 	sin_port;		//Port number in network byte order
+	struct in_addr 	sin_addr;		//IP addr in network byte order
+	unsigned char 	sin_zero[8];	//pad to sizeof(struct socckaddr)
+};
+
+typedef struct sockaddr SA; //For casting purposes
+
 int main(){
 	int listenfd, connfd, port, clentlen;
 	struct sockaddr_in clientaddr;
@@ -9,17 +35,17 @@ int main(){
 	}
 	port = atoi(argv[1]);
 
-	/* customize open_listenfd */
+	/* TODO customize open_listenfd */
 	listenfd = Open_listenfd(port);
 
 	while(1){
 		clientlen = sizeof(clientaddr);
 
-		/* customize accept */
+		/* TODO customize accept */
 		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 		doit(connfd);
 
-		/* customize close */
+		/* TODO customize close */
 		Close(connfd);
 	}
 
