1398e13a168265a79efe6554a84d4e2d5bdd67b8
Eric <esummers@andrew.cmu.edu>
2012-04-09 17:16:10 -0400

    clienterror

--- AwesomeServer.c
+++ AwesomeServer.c
@@ -112,3 +112,23 @@ void doit(int fd){
 	}
 }
 
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg){
+	char buf[MAXLINE], body[MAXLINE];
+
+	/* build HTTP response body (html) */
+	sprintf(body, "<html><title>Awesome Error</title>");
+	sprintf(body, "%s<body bgcolor=""ffffff"">\f\n", body);
+	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+	sprintf(body, "%s<hr><em>The Awesome Web server</em>\r\n", body);
+
+	/* Print the HTTP response */
+	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+	/* TODO customize Rio_writen */
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-type: text/html\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+	Rio_writen(fd, buf, strlen(buf));
+	Rio_writen(fd, body, strlen(body));
+}
