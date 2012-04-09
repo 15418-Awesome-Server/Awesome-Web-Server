7f6991ff032e84e374aa00ccb9bf2146c7a9f43e
Eric <esummers@andrew.cmu.edu>
2012-04-09 16:38:36 -0400

    openlistenfd

--- AwesomeServer.c
+++ AwesomeServer.c
@@ -35,8 +35,8 @@ int main(){
 	}
 	port = atoi(argv[1]);
 
-	/* TODO customize open_listenfd */
-	listenfd = Open_listenfd(port);
+	/* TODO customize open_listenfd? */
+	listenfd = open_listenfd(port);
 
 	while(1){
 		clientlen = sizeof(clientaddr);
@@ -50,4 +50,31 @@ int main(){
 	}
 
 	exit(0);
+}
+
+open_listenfd(int port){
+	int listenfd, optval = 1;
+	struct sockaddr_in serveraddr;
+
+	/* create socket descriptor */
+	if((listenfd = socket(AF_INET, SOCKSTREAM, 0)) < 0)
+		return -1;
+
+	/* Eliminate "Address already in use" error from bind */
+	if(setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int)))
+		return -1;
+
+	/* listenfd will be an endpoint for all requests to port on any IP address for this host */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+	if(bind(listenfd, (SA*)&serveraddr, sizeof(serveraddr)) < 0)
+		return -1;
+
+	/* Make it a listening socket ready to accept connection requests */
+	if(listen(listenfd, LISTENQ) < 0)
+		return -1;
+
+	return listenfd;
 }
